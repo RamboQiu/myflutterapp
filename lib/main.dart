76854e4d387af5bec2784e73d8c5ed52833ea9ab
裘俊云 <hzqiujunyun@corp.netease.com>
2019-07-10 22:29:26 +0800

    [第四章] 常用组件-容器组件、图片组件、文本组件、图标及按钮组件

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,124 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
 
-
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = new Container(
-      padding: const EdgeInsets.all(32.0),
-      child: new Row(
-        children: <Widget>[
-          new Expanded(
-            child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return MaterialApp(
+      title: '容器组件示例',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('容器组件示例'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Row (
               children: <Widget>[
-                new Container(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: new Text(
-                    'Oeschine Lake Campground',
-                    style: new TextStyle(
-                      fontWeight: FontWeight.bold,
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    border: new Border.all(
+                      color: Colors.grey,
+                      width: 8.0,
                     ),
+                    borderRadius: const BorderRadius.all(const Radius.circular(8.0)),
+                  ),
+                  child: Text(
+                    'Flutter',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 28),
                   ),
                 ),
-                new Text(
-                  'KanderSteg, Switzerland',
-                  style: new TextStyle(
-                    color: Colors.grey[500],
-                  ),
+                Image.network(
+                  'https://cdn.jsdelivr.net/gh/flutterchina/website@1.0/images/flutter-mark-square-100.png',
+                  fit: BoxFit.fitWidth,
                 ),
               ],
             ),
-          ),
-          new Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          new Text('41'),
-        ],
-      ),
-    );
-
-    Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          _buildButtonColumn(color, Icons.call, 'CALL'),
-          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-          _buildButtonColumn(color, Icons.share, 'SHARE'),
-        ],
-      ),
-    );
-    
-    Widget textSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Text('''
-      Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
-      ''',
-      softWrap: true,
-      ),
-    );
-
-    return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter layout demo'),
-        ),
-        body: ListView(
-          children: <Widget>[
-            Image.asset(
-              'images/lake.jpg',
-              width: 600,
-              height: 240,
-              fit: BoxFit.cover,
+            Row(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text(
+                      '红色+黑色删除线+25号',
+                      style: TextStyle(
+                        color: const Color(0xffff0000),
+                        decoration: TextDecoration.lineThrough,
+                        decorationColor: const Color(0xff000000),
+                        fontSize: 25,
+                        letterSpacing: 10,
+                      ),
+                    ),
+                    Text(
+                      '橙色 + 下划线 +24 号',
+                      style: TextStyle(
+                        color: const Color(0xffff9900),
+                        decoration: TextDecoration.underline,
+                        fontSize: 24,
+                      ),
+                    ),
+                    Text(
+                      '虚线上划线 +23 号 +倾斜',
+                      style: TextStyle(
+                        decoration: TextDecoration.overline,
+                        decorationStyle: TextDecorationStyle.dashed,
+                        fontSize: 23.0,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    Text(
+                      '24号+加粗',
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 6,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            titleSection,
-            buttonSection,
-            textSection,
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.phone,
+                  color: Colors.green[500],
+                  size: 80,
+                ),
+                IconButton(
+                    icon: Icon(
+                      Icons.volume_up,
+                      size: 48,
+                    ),
+                    tooltip: '按下操作',
+                    onPressed: () {
+                      print('按下操作');
+                    },
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    
+                  },
+                  child: Text('RaisedButton组件'),
+                ),
+                    
+
+              ],
+            )
           ],
         ),
+
+
+
+
+
       ),
     );
   }
-  
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
+
 }
