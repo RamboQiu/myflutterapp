99912f3e242764d434b6eb74a9c220e3d75dec76
裘俊云 <hzqiujunyun@corp.netease.com>
2019-10-24 11:23:51 +0800

    复习

--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,183 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new LoginPage());
+class ImageDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Center(
+      child: new Image.network(
+        'https://yanxuan.nosdn.127.net/097e2254bc37d7c53f426d058117faf9.gif',
+        fit: BoxFit.fitWidth,
+      ),
+    );
+  }
+
 }
 
-class LoginPage extends StatefulWidget {
+class LayoutDemo extends StatelessWidget {
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return new _LoginPageState();
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('图标按钮组件示例'),
+      ),
+      body: new Center(
+        child: new IconButton(
+            icon: Icon(Icons.volume_up,size: 48,),
+            onPressed: () {
+              print('press down');
+            },
+            tooltip: 'press tooltip',
+        ),
+      ),
+    );
   }
 }
 
-class _LoginPageState extends State<LoginPage> {
-
-  /// 全局 Key 用来获取Form表单组件
-  GlobalKey<FormState> loginKey = new GlobalKey<FormState>();
-
-  /// 用户名
-  String userName;
-
-  /// 密码
-  String password;
-
-  void login() {
-    // 读取当前的Form状态
-    var loginForm = loginKey.currentState;
-
-    // 验证Form表单
-    if (loginForm.validate()) {
-      loginForm.save();
-      print('userName:' + userName + ' password:' + password);
-    }
-
-  }
+class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new MaterialApp(
-      title: 'Form表单示例',
+      title: 'RaiseButton',
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Form表单示例'),
+          title: new Text('RaiseButton1'),
         ),
-        body: new Column(
-          children: <Widget>[
-            new Container(
-              padding: const EdgeInsets.all(16.0),
-              child: new Form(
-                  key: loginKey,
-                  child: new Column(
-                    children: <Widget>[
-                      new TextFormField(
-                        decoration: new InputDecoration(
-                          labelText: '请输入用户名',
-                        ),
-                        onSaved: (value) {
-                          userName = value;
-                        },
-                        onFieldSubmitted: (value) {
+        body: new Center(
+          child: new RaisedButton(
+              onPressed: () {
+                print('press action');
+              },
+              child: new Text('RaiseButton weidget'),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
-                        },
+class MyListView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: '基础列表',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ListView'),
+        ),
+        body: new ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            Container(
+              width: 160,
+              color: Colors.lightBlue,
+            ),
+            Container(
+              width: 160,
+              color: Colors.amber,
+            ),
+            Container(
+              width: 160,
+              color: Colors.green,
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    '水平',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 36,
+                    ),
+                  ),
+                  Text(
+                      '列表',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 36,
                       ),
-                      new TextFormField(
-                        decoration: new InputDecoration(
-                          labelText: '请输入密码',
-                        ),
-                        obscureText: true,
-                        validator: (value) {
-                          return value.length < 6 ? '密码长度不够6位' : null;
-                        },
-                        onSaved: (value) {
-                          password = value;
-                        },
-                      ),
-                    ],
-                  )
+                  ),
+                  Icon(Icons.list),
+                ],
               ),
             ),
-            new SizedBox(
-              width: 340.0,
-              height: 42,
-              child: new RaisedButton(
-                onPressed: login,
-                child: new Text(
-                  '登录',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                  ),
-                ),
-              ),
+            Container(
+              width: 160,
+              color: Colors.deepPurpleAccent,
+            ),
+            Container(
+              width: 160,
+              color: Colors.black,
+            ),
+            Container(
+              width: 160,
+              color: Colors.pinkAccent,
             ),
           ],
         ),
       ),
     );
   }
+}
+
+class LongListView extends StatelessWidget {
+
+  final List<String> items;
+
+  LongListView(this.items);
+
+//  LongListView({Key key, @required this.items}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
+
+class MyGridView extends StatelessWidget {
+  final title = '网格列表示例';
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      title: title,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(title),
+        ),
+        body: new GridView.count(
+          crossAxisCount: 3,
+          primary: false,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 30,
+          children: <Widget>[
+            const Text('01'),
+            const Text('02'),
+            const Text('03'),
+            const Text('11'),
+            const Text('12'),
+            const Text('13'),
+            const Text('21'),
+            const Text('21'),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 
+
+void main() {
+  runApp(new MaterialApp(
+      title: 'Image Demo',
+      home: new MyGridView(),
+    )
+  );
 }
